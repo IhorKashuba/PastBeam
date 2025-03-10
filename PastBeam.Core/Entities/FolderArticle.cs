8294efac8228e4fe6666bfe255acbc46695e316d
Rediont <mishunyaka14@gmail.com>
2025-03-10 23:05:18 +0200

    Added basic entity framework core models (connection link later)

---
+++ PastBeam.Core/Entities/FolderArticle.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities;
+
+public class FolderArticle
+{
+    [ForeignKey("Folder")]
+    public int FolderId { get; set; }
+
+    [ForeignKey("Article")]
+    public int ArticleId { get; set; }
+
+    public Folder Folder { get; set; } = null!;
+    public Article Article { get; set; } = null!;
+}
+
