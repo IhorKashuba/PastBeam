babdb5bc35004b2979596c502e883d093aee34d2
Ігор Кашуба <127357041+IhorKashuba@users.noreply.github.com>
2025-03-11 14:33:38 +0200

    Added logger

---
+++ PastBeam.Infrastructure/Loger/Loger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Serilog;
+
+namespace PastBeam.Infrastructure.Logger
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+        }
+
+        public static void LogToFile(string message)
+        {
+            Log.Information(message);
+            Log.CloseAndFlush(); // Забезпечує коректний запис у файл
+        }
+    }
+}
