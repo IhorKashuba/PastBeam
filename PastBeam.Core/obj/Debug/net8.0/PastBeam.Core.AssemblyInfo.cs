a4f47186c4f369d0a8be7f54dbac95a3d2e6e754
Rediont <mishunyaka14@gmail.com>
2025-03-17 18:26:36 +0200

    test

--- PastBeam.Core/obj/Debug/net8.0/PastBeam.Core.AssemblyInfo.cs
+++ PastBeam.Core/obj/Debug/net8.0/PastBeam.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PastBeam.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c59d4f145d543d431e826f3180d059d2d6673f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PastBeam.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PastBeam.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
