b29cd0a2eabf993e1b3e01fc4c2eb656b0f3498a
Rediont <mishunyaka14@gmail.com>
2025-03-23 09:26:14 +0200

    test

---
+++ PastBeam.Application1/ArticleService.cs
@@ -0,0 +1,77 @@
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using PastBeam.Core.Interfaces;
+using ILogger = PastBeam.Infrastructure.Logger.ILogger;
+
+namespace PastBeam.Application.Services
+{
+    public class ArticleService
+    {
+        private readonly IArticleRepository repository;
+
+        private readonly ILogger _logger;
+
+        public ArticleService(IArticleRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<Article>> GetAllArticlesAsync()
+        {
+            return await this.repository.GetAllAsync();
+        }
+
+        public async Task<Article?> GetArticleByIdAsync(int id)
+        {
+            return await this.repository.GetByIdAsync(id);
+        }
+
+        public async Task<Article?> CreateArticle(Article article)
+        {
+            try
+            {
+                this.repository.CreateArticleAsync(article);
+                return article;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogToFile($"Error while creating article {ex}");
+                throw;
+            }
+        }
+
+        public async Task<Article?> UpdateArticleAsync(int id, string? title = null, string? content = null, List<string>? tags = null)
+        {
+            var article = await repository.GetByIdAsync(id);
+
+            if (article == null)
+                return null;
+
+            if (title != null)
+            {
+                article.Title = title;
+            }
+            if (content != null)
+            {
+                article.Content = content;
+            }
+            if (tags != null)
+            {
+                article.Tags = tags;
+            }
+
+            article.UpdatedAt = DateTime.UtcNow;
+
+            try
+            {
+                await repository.UpdateArticleAsync(article);
+                return article;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogToFile($"Error while updating article {ex}");
+                throw;
+            }
+        }
+    }
+}
