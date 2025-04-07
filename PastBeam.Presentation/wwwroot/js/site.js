c7a7e7cdd73a7343cc8924bd3b6f4a6b6d9e328f
Rediont <mishunyaka14@gmail.com>
2025-04-07 18:09:07 +0300

    added sidebar to main page

--- PastBeam.Presentation/wwwroot/js/site.js
+++ PastBeam.Presentation/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+const sidebar = document.getElementById("sidebar");
+const menuToggle = document.getElementById("menu-toggle");
+const menuClose = document.getElementById("menu-close");
+
+menuToggle.addEventListener("click", function () {
+    sidebar.classList.toggle("open");
+});
+
+menuClose.addEventListener("click", function () {
+    sidebar.classList.remove("open");
+});
